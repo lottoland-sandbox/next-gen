fcc16555fdfe07c432e1ebd064ea741687426f61
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2024-10-21 16:11:24 +0200

    Update global-scripts.js

--- scripts/global-scripts.js
+++ scripts/global-scripts.js
@@ -151,5 +151,28 @@ document.getElementById('closePopup').addEventListener('click', function() {
 });
 
 
+function setUTMCookies() {
+            // Function to set a cookie
+            function setCookie(name, value, days) {
+                const d = new Date();
+                d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+                const expires = "expires=" + d.toUTCString();
+                document.cookie = name + "=" + (value || "") + ";" + expires + ";path=/";
+            }
 
+            // Get the URL parameters
+            const urlParams = new URLSearchParams(window.location.search);
+
+            // Define the parameters you want to track
+            const parametersToTrack = ['utm_campaign', 'utm_source', 'utm_medium', 'utm_term', 'dest', 'dept'];
+
+            // Set cookies for each parameter
+            parametersToTrack.forEach(param => {
+                const value = urlParams.get(param);
+                if (value) {
+                    setCookie(param, value, 30); // Set cookie for 30 days
+                }
+            });
+        }
+       
 
