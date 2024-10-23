80a21f1ba923a81c04d3a90da535f3df730f24b2
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2024-10-23 10:44:20 +0200

    Update header-script.js

--- scripts/header-script.js
+++ scripts/header-script.js
@@ -12,6 +12,13 @@ var user1 = {
     'Player Tier': 'VIP',
     'Balance': '10.23',
     'Logged In': 'yes',
+
+    'utm_campaign': 'halloween',
+    'utm_source': 'facebook',
+    'utm_medium': 'social',
+    'utm_term': 'ghostofdead_50',
+    'department': 'acquisition_organic',
+    'destination': 'landing',
 };
 
 var user2 = {
@@ -22,6 +29,13 @@ var user2 = {
     'Player Tier': 'NON VIP',
     'Balance': '102.23',
     'Logged In': 'yes',
+
+    'utm_campaign': 'christmas',
+    'utm_source': 'groupon',
+    'utm_medium': 'affiliates',
+    'utm_term': 'advent_calendar',
+    'department': 'acquisition_performance',
+    'destination': 'landing',
 };
 
 var user3 = {
@@ -32,6 +46,13 @@ var user3 = {
     'Player Tier': 'GOLD',
     'Balance': '0.22',
     'Logged In': 'yes',
+
+    'utm_campaign': 'halloween',
+    'utm_source': 'google',
+    'utm_medium': 'ppc',
+    'utm_term': 'ghostofdead_50',
+    'department': 'acquisition_performance',
+    'destination': 'landing',
 };
 
 var user4 = {
@@ -42,6 +63,13 @@ var user4 = {
     'Player Tier': 'PLATINUM',
     'Balance': '9.11',
     'Logged In': 'yes',
+
+    'utm_campaign': 'casino',
+    'utm_source': 'myaffiliates',
+    'utm_medium': 'coop',
+    'utm_term': 'big_bass_bonanza',
+    'department': 'acquisition_performance',
+    'destination': 'landing',
 };
 
 // Function to select a random payload
