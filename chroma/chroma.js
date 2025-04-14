dabfaeda6cefc47f5c5f9fa99a91e7b4402fbb9b
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2025-04-14 10:44:22 +0200

    Update chroma.js

--- chroma/chroma.js
+++ chroma/chroma.js
@@ -1,4 +1,55 @@
-function logout() {
+
+
+
+
+function registrationSuccess() {
+
+}
+
+function registrationFailure() {
+
+}
+
+function loginSuccess() {
+
+    // ** IDENTITY ***
+    FS('setIdentity',
+    uid:'123456',
+    properties: {
+      "player-number":'123456',
+      "account-status":'SELFBAN',
+      "player-tier": 'SILVER',
+      "verification-status": 'PENDING',
+      "identification-method": 'login'
+    }) 
+
+    // ** LOGIN EVENT ***
+    FS('trackEvent', {
+      name: 'Login | Success',
+      properties: {
+        "type": 'standard',
+        "2fa": 'none'
+        "attempt": 1,
+      }
+    });
+     alert('Log In Success');
+}
+
+function loginFailure() {
+    FS('trackEvent', {
+      name: 'Log In | Failure',
+      properties: {
+        "type": 'standard',
+        "2fa": 'none'
+        "attempt": 1,
+        "error": "InvalidCredentials"
+      }
+    }); 
+    alert('Log In Failure');
+}
+
+
+function logoutSuccess() {
     FS('trackEvent', {
         name: 'Logout | Success',
         properties: {
