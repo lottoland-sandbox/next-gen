eb0a4abbc57c443cd60d9b1638a4e60600ab6f42
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2025-04-14 10:54:41 +0200

    Update chroma.js

--- chroma/scripts/chroma.js
+++ chroma/scripts/chroma.js
@@ -11,51 +11,54 @@ function registrationFailure() {
 }
 
 function loginSuccess() {
-
     // ** IDENTITY ***
-    FS('setIdentity',
-    uid:'123456',
-    properties: {
-      "player-number":'123456',
-      "account-status":'SELFBAN',
-      "player-tier": 'SILVER',
-      "verification-status": 'PENDING',
-      "identification-method": 'login'
-    }) 
+    FS('setIdentity', {
+        uid: '123456',
+        properties: {
+            "player-number": '123456',
+            "account-status": 'SELFBAN',
+            "player-tier": 'SILVER',
+            "verification-status": 'PENDING',
+            "identification-method": 'login'
+        }
+    });
 
     // ** LOGIN EVENT ***
     FS('trackEvent', {
-      name: 'Login | Success',
-      properties: {
-        "type": 'standard',
-        "2fa": 'none'
-        "attempt": 1,
-      }
+        name: 'Login | Success',
+        properties: {
+            "type": 'standard',
+            "2fa": 'none',
+            "attempt": 1
+        }
     });
-     alert('Log In Success');
+
+    alert('Log In Success');
 }
 
 function loginFailure() {
     FS('trackEvent', {
-      name: 'Log In | Failure',
-      properties: {
-        "type": 'standard',
-        "2fa": 'none'
-        "attempt": 1,
-        "error": "InvalidCredentials"
-      }
-    }); 
+        name: 'Log In | Failure',
+        properties: {
+            "type": 'standard',
+            "2fa": 'none',
+            "attempt": 1,
+            "error": "InvalidCredentials"
+        }
+    });
+
     alert('Log In Failure');
 }
-
 
 function logoutSuccess() {
     FS('trackEvent', {
         name: 'Logout | Success',
         properties: {
-            method: 'manual',    
+            method: 'manual'
         }
-     });
-    FS('setIdentity', { anonymous: true }); 
+    });
+
+    FS('setIdentity', { anonymous: true });
+
     alert('Logged out');
-} 
+}
