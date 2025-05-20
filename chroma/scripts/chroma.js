493afc5c6f9323506fcaa9176eb1ac00af06b59a
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2025-05-20 13:00:34 +0200

    Update chroma.js

--- chroma/scripts/chroma.js
+++ chroma/scripts/chroma.js
@@ -1,3 +1,19 @@
+function accountRegistrationSuccess() {
+ FS('trackEvent', {
+        name: 'Account | Registration | Success',
+        properties: {
+            
+            accountStatus: 'REGISTERED',
+            playerTier: 'NON-VIP',
+            verificationStatus: 'PENDING',
+            
+        }
+    });    
+}
+
+
+
+
 function registrationSuccess() {
     // Implement registration success logic here if needed
 }
